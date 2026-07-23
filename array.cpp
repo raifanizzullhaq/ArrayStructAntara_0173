07300e32c5b10e35cb3af82d476e2f51b345fe85
raifanizzullhaq <raifanizzulhaq3>
2026-07-23 14:23:26 +0700

    Add array initialization and user input functionality

--- array.cpp
+++ array.cpp
@@ -8,5 +8,22 @@ int main()
     //deklarasi tanpa inisialisasi
     int datamu[5];
 
+    //cara akses (membaca atau mengambil isi array)
+    cout << "Isi data kedua : " << dataku[1] << endl;
+    // mengisi array
+    dataku[1] = 200;
+    cout << "isi data kedua setelah diubah : " << dataku[1] << endl;
 
-}
+    //mengisi data dengan perintah satu persatu
+    cout << "isikan data pertama : ";
+    cin >> datamu [0];
+    cout << "isikan data kedua : ";
+    cin >> datamu [1];
+    cout << "isikan data ketiga : ";
+    cin >> datamu [2];
+    cout << "isikan data keempat : ";
+    cin >> datamu [3];
+    cout << "isikan data kelima : ";
+    cin >> datamu [4];
+    cout << endl;
+
